1f475b37794816676e8b44c2e350fae05c806788
sam-twdy <stweedy.st@gmail.com>
2024-02-26 22:15:32 -0500

    final commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,10 @@
 'use client';
-import styled from '@emotion/styled';
+import { useRef, useState } from 'react';
+import styles from './page.module.scss';
 import Image from 'next/image';
+import Lenis from '@studio-freight/lenis';
+import { useTransform, useScroll, motion } from 'framer-motion';
+import { useGSAP } from '@gsap/react';
 
 const images = [
   '1.jpg',
@@ -17,77 +21,65 @@ const images = [
   '12.jpg',
 ];
 
-const Main = styled.main`
-  width: 100%;
-`;
-
-const Gallery = styled.div`
-  height: 175vh;
-  background: rgb(45, 45, 45);
-  position: relative;
-  display: flex;
-  gap: 2vw;
-  padding: 2vw;
-  box-sizing: border-box;
-  overflow: hidden;
-`;
-
-const Container = styled.div`
-  position: relative;
-  height: 100%;
-  width: 25%;
-  min-width: 250px;
-  display: flex;
-  flex-direction: column;
-  gap: 2vw;
-`;
-
-const ImageContainer = styled.div`
-  height: 100%;
-  width: 100%;
-  position: relative;
-  border-radius: 1vw;
-  overflow: hidden;
-  img {
-    object-fit: cover;
-  }
-  &:nth-of-type(1) {
-    top: -45%;
-  }
-  &:nth-of-type(2) {
-    top: -95%;
-  }
-  &:nth-of-type(3) {
-    top: -45%;
-  }
-  &:nth-of-type(4) {
-    top: -75%;
-  }
-`;
-
-const Column = ({ images }) => {
+const Column = ({ images, y }) => {
   return (
-    <Container>
-      {images.map((src, index) => {
+    <motion.div className={styles.column} style={{ y }}>
+      {images.map((src, i) => {
         return (
-          <ImageContainer key={index}>
-            <Image src={`/images/${src}`} fill alt='image' />
-          </ImageContainer>
+          <div key={i} className={styles.imageContainer}>
+            <Image src={`/images/${src}`} alt='image' fill />
+          </div>
         );
       })}
-    </Container>
+    </motion.div>
   );
 };
 
 export default function Home() {
+  const gallery = useRef(null);
+  const container = useRef(null);
+  const [dimension, setDimension] = useState({ width: 0, height: 0 });
+
+  const { scrollYProgress } = useScroll({
+    target: gallery,
+    offset: ['start end', 'end start'],
+  });
+  const { height } = dimension;
+  const y = useTransform(scrollYProgress, [0, 1], [0, height * 2]);
+  const y2 = useTransform(scrollYProgress, [0, 1], [0, height * 3.3]);
+  const y3 = useTransform(scrollYProgress, [0, 1], [0, height * 1.25]);
+  const y4 = useTransform(scrollYProgress, [0, 1], [0, height * 3]);
+
+  useGSAP(
+    () => {
+      const lenis = new Lenis();
+      const raf = (time) => {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+      };
+      const resize = () => {
+        setDimension({ width: window.innerWidth, height: window.innerHeight });
+      };
+      window.addEventListener('resize', resize);
+      requestAnimationFrame(raf);
+      resize();
+      return () => {
+        window.removeEventListener('resize', resize);
+      };
+    },
+    { scope: container },
+  );
+
   return (
-    <Main>
-      <Gallery>
-        <Column images={[images[0], images[1], images[2]]} />
-        <Column images={[images[3], images[4], images[5]]} />
-        <Column images={[images[6], images[7], images[8]]} />
-        <Column images={[images[9], images[10], images[11]]} />
-      </Gallery>
-    </Main>
+    <main ref={container} className={styles.main}>
+      <div className={styles.spacer}></div>
+      <div ref={gallery} className={styles.gallery}>
+        <Column images={[images[0], images[1], images[2]]} y={y} />
+        <Column images={[images[3], images[4], images[5]]} y={y2} />
+        <Column images={[images[6], images[7], images[8]]} y={y3} />
+        <Column images={[images[9], images[10], images[11]]} y={y4} />
+      </div>
+      <div className={styles.spacer}></div>
+    </main>
   );
 }
